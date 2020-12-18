8e49301779715a7e6f1d4c2c65d1b5e69b8632f4
stevehy_shih <stevehy_shih@compal.com>
2020-12-18 10:54:59 +0800

    [GECKOT] ADD MAIN CLASS AI SERVICE

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 18 10:50:22 2020
+
+@author: steve
+"""
+import cv2
+import scipy
+def addALG_LENTH():
+    return()
+def addALG_DENSITY():
+    return()
+class GeckoAIService():
+     def __init__(self, length,density,):
+          self.length = length
+
+    
